e7b8c789b6107c6105d0b016558ba81cc712442b
Ahmad N. F <fahmadnaufal@gmail.com>
2016-03-09 00:41:59 +0700

    add camera intent

--- app/src/main/java/com/ahmadnaufalfarhan/guestguessitb/MainActivity.java
+++ app/src/main/java/com/ahmadnaufalfarhan/guestguessitb/MainActivity.java
@@ -2,6 +2,7 @@ package com.ahmadnaufalfarhan.guestguessitb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -48,4 +49,14 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
 }
