6c929eec7610eb5a2b7dabb8e55ac2e55a88e7a5
Ahmad N. F <fahmadnaufal@gmail.com>
2016-03-09 00:23:24 +0700

    added maps and api key

--- app/src/main/java/com/ahmadnaufalfarhan/guestguessitb/MainActivity.java
+++ app/src/main/java/com/ahmadnaufalfarhan/guestguessitb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ahmadnaufalfarhan.guestguessitb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -48,5 +42,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startMapsActivity(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
